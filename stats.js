997f30f83024a695b835a89873644974c1a97891
romeroschwarz <167591320+romeroschwarz@users.noreply.github.com>
2024-04-21 00:40:31 -0400

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,5 +1,5 @@
 var data = [];
-// 0:hurr, 1:sev. storm, 2:snowst, 3:flood, 4:torna, 5:mu/land, 6:fire, 7:sub hous risk rank, 8:pov rate, 9:med income, 10:num fed sub prop, 11:delta hpi, 12:hpi2018, 13:hpi 2022
+// 0:hurr, 1:sev. storm, 2:snowst, 3:flood, 4:torna, 5:mu/land, 6:fire, 7:sub hous risk rank, 8:pov rate, 9:med income, 10:num fed sub prop, 11:delta hpi, 12:hpi2018, 13:hpi 2022, 14:name
 
 
 
@@ -38,6 +38,7 @@ function getData(allText,fipsCode) {
             data.push(line[23]); //delta hpi
             data.push(line[21]); //hpi2108
             data.push(line[22]); //hpi2022
+            data.push(line[3]); //name
             break;
         }
     }
