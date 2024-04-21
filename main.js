bea812c973f36e189f2be55f356ce19e011bd295
romeroschwarz <167591320+romeroschwarz@users.noreply.github.com>
2024-04-20 23:50:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function processData(allText) {
         dataByFIPS[lines[i][0].split(':')[1]] = [lines[i][8].split(':')[1],lines[i][9].split(':')[1],lines[i][10].split(':')[1],lines[i][11].split(':')[1],lines[i][12].split(':')[1],lines[i][13].split(':')[1],lines[i][20].split(':')[1],lines[i][5].split(':')[1],lines[i][6].split(':')[1],lines[i][18].split(':')[1],lines[i][23].split(':')[1],lines[i][21].split(':')[1],lines[i][22].split(':')[1]];
     }
     categories.sort();
-    console.log(dataByFIPS.lines[i][3].split(':')[1]);
+    console.log(dataByFIPS.(lines[i][3].split(':')[1]));
 }
 
 
