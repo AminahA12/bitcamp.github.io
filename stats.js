3d394efca7c2ea01449a5ba64da26a5ed4058e1f
romeroschwarz <167591320+romeroschwarz@users.noreply.github.com>
2024-04-21 00:06:44 -0400

    Create stats.js

---
+++ stats.js
@@ -0,0 +1,5 @@
+// 0:hurr, 1:sev. storm, 2:snowst, 3:flood, 4:torna, 5:mu/land, 6:fire, 7:sub hous risk rank, 8:pov rate, 9:med income, 10:num fed sub prop, 11:delta hpi, 12:hpi2018, 13:hpi 2022
+
+$(document).ready(function() {
+    console.log(dataByFIPS);
+});
