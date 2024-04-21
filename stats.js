4f8b71c6c6df009964c4ff08ca13667df2b9b2a1
romeroschwarz <167591320+romeroschwarz@users.noreply.github.com>
2024-04-21 00:52:15 -0400

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,4 +1,4 @@
-var data = [];
+var countyData = [];
 // 0:hurr, 1:sev. storm, 2:snowst, 3:flood, 4:torna, 5:mu/land, 6:fire, 7:sub hous risk rank, 8:pov rate, 9:med income, 10:num fed sub prop, 11:delta hpi, 12:hpi2018, 13:hpi 2022, 14:name
 
 
@@ -12,6 +12,9 @@ $(document).ready(function() {
             var urlParams = new URLSearchParams(window.location.search);
             var fip = urlParams.get('fips');
             getData(data,fip);
+
+            document.getElementById('county').innerHTML = data[14];
+            
         }
      });
 });
@@ -24,25 +27,25 @@ function getData(allText,fipsCode) {
     for (var i=1; i<allTextLines.length; i++) {
         var line = allTextLines[i].split(',');
         if (line[0] == fipsCode) {
-            data.push(line[8]); //hurr
-            data.push(line[9]); //sev storm
-            data.push(line[10]); //snow
-            data.push(line[7]); //flood
-            data.push(line[11]); //torn
-            data.push(line[12]); //mud/land
-            data.push(line[13]); //fire
-            data.push(line[20]); //sub hous risk rank
-            data.push(line[15]); //pov rate
-            data.push(line[6]); //med income
-            data.push(line[18]); //num fed sub prop
-            data.push(line[23]); //delta hpi
-            data.push(line[21]); //hpi2108
-            data.push(line[22]); //hpi2022
-            data.push(line[3]); //name
+            countyData.push(line[8]); //hurr
+            countyData.push(line[9]); //sev storm
+            countyData.push(line[10]); //snow
+            countyData.push(line[7]); //flood
+            countyData.push(line[11]); //torn
+            countyData.push(line[12]); //mud/land
+            countyData.push(line[13]); //fire
+            countyData.push(line[20]); //sub hous risk rank
+            countyData.push(line[15]); //pov rate
+            countyData.push(line[6]); //med income
+            countyData.push(line[18]); //num fed sub prop
+            countyData.push(line[23]); //delta hpi
+            countyData.push(line[21]); //hpi2108
+            countyData.push(line[22]); //hpi2022
+            countyData.push(line[3]); //name
             break;
         }
     }
-    console.log(data);
+    console.log(countyData);
 }
 
 
